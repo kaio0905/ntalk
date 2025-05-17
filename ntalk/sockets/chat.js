b890871dbbcdecb998fc174c5cf7e7d856aa9c1a
Cleiton Moura <66132307+Pquar@users.noreply.github.com>
2025-05-17 01:22:36 -0300

    fix: update a lib func

--- ntalk/sockets/chat.js
+++ ntalk/sockets/chat.js
@@ -1,49 +1,43 @@
 module.exports = function (io) {
   var crypto = require("crypto"),
-    md5 = crypto.createHash("md5"),
     sockets = io.sockets;
-  sockets.on("connection", function (client) {
-    var session = client.handshake.session,
-      usuario = session.usuario;
-    client.set('email', usuario.email);
 
-    var onlines = sockets.clients();
-    onlines.forEach(function (online) {
-      var online = sockets.sockets[online.id];
-      online.get('email', function (err, email) {
-        client.emit('notify-onlines', email);
-        client.broadcast.emit('notify-onlines', email);
-      });
+  sockets.on("connection", function (client) {
+    var session = client.request.session,
+      usuario = session.usuario;
+    client.data.email = usuario.email;
+
+    sockets.sockets.forEach(function (onlineSocket) {
+      client.emit("notify-onlines", onlineSocket.data.email);
+      client.broadcast.emit("notify-onlines", onlineSocket.data.email);
     });
 
     client.on("send-server", function (msg) {
-      var msg = "<b>" + usuario.nome + ":</b> " + msg + "<br>";
-      client.get("sala", function (erro, sala) {
-        var data = { email: usuario.email, sala: sala };
-        client.broadcast.emit("new-message", data);
-        sockets.in(sala).emit("send-client", msg);
-      });
+      var mensagem = "<b>" + usuario.nome + ":</b> " + msg + "<br>";
+      var sala = client.data.sala;
+      var data = { email: usuario.email, sala: sala };
+      client.broadcast.emit("new-message", data);
+      sockets.in(sala).emit("send-client", mensagem);
     });
 
     client.on("join", function (sala) {
       if (sala) {
         sala = sala.replace("?", "");
       } else {
-        var timestamp = new Data().toString();
+        var timestamp = new Date().toString();
         var md5 = crypto.createHash("md5");
         sala = md5.update(timestamp).digest("hex");
       }
-      client.set("sala", sala);
+      client.data.sala = sala;
       client.join(sala);
     });
 
     client.on("disconnect", function () {
-      client.get("sala", function (erro, sala) {
-        var msg = "<b>" + usuario.nome + ":</b> saiu.<br>";
-        client.broadcast.emit('notify-offline', usuario.email);
-        sockets.in(sala).emit('send-client', msg);
-        client.leave(sala);
-      });
+      var sala = client.data.sala;
+      var msg = "<b>" + usuario.nome + ":</b> saiu.<br>";
+      client.broadcast.emit("notify-offline", usuario.email);
+      sockets.in(sala).emit("send-client", msg);
+      client.leave(sala);
     });
   });
 };
